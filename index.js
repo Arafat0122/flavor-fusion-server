f5442002a3ae4926d6ff8c2fcd33cd62994b4e28
Arafat0122 <soyebarafat0122@yahoo.com>
2024-05-13 02:28:05 +0600

    single food get by id  from mongoDB

--- index.js
+++ index.js
@@ -34,8 +34,13 @@ async function run() {
             res.send(result);
         })
 
-        
 
+        app.get('/foods/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await foodCollection.findOne(query);
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
