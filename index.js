8504a1641661e23128429ff14fdfe46be6fcc9df
Arafat0122 <soyebarafat0122@yahoo.com>
2024-05-15 09:45:18 +0600

    server site ready

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -16,6 +17,7 @@ app.use(
     })
 );
 app.use(express.json());
+app.use(cookieParser());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kunr0xg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -29,19 +31,38 @@ const client = new MongoClient(uri, {
     }
 });
 
+const logger = (req, res, next) => {
+    console.log(req.method, req.url);
+    next();
+}
+
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token;
+
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.user = decoded;
+        next();
+    })
+}
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();//remove this before sending to production
 
         const foodCollection = client.db('flavorFusion').collection('food');
         const purchaseFoodCollection = client.db('flavorFusion').collection('purchaseFood');
         const galleryCollection = client.db('flavorFusion').collection('gallery');
 
         // Auth related api
-        app.post('/jwt', async (req, res) => {
+        app.post('/jwt', logger, async (req, res) => {
             const user = req.body;
-            console.log(user)
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
             res.cookie('token', token, {
                 httpOnly: true,
@@ -52,15 +73,15 @@ async function run() {
 
         app.post('/logout', async (req, res) => {
             const user = req.body;
-
-            console.log(user)
             res.clearCookie('token', { maxAge: 0 }).send({ success: true })
         })
 
 
         //Purchase Data api
-
-        app.get('/purchaseFood', async (req, res) => {
+        app.get('/purchaseFood', verifyToken, async (req, res) => {
+            if (req.user?.email !== req.query?.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email }
@@ -78,16 +99,16 @@ async function run() {
         // As wanted
         app.post('/purchaseFood', async (req, res) => {
             const purchaseFood = req.body;
+            const foodId = purchaseFood.foodId;
 
-            // Increment purchase count
-            const foodId = purchaseFood.foodId; // Assuming you have a unique food identifier in your purchaseFood object
+            // Increment purchase count for the purchased food item
             await foodCollection.updateOne({ _id: new ObjectId(foodId) }, { $inc: { purchaseCount: 1 } });
-            console.log(foodId)
+
             // Insert purchase data
             const result = await purchaseFoodCollection.insertOne(purchaseFood);
             res.send(result);
-            console.log(result)
-        })
+        });
+
 
         app.delete('/purchaseFood/:id', async (req, res) => {
             const id = req.params.id;
@@ -100,6 +121,15 @@ async function run() {
         //Foods Data api
 
         app.get('/foods', async (req, res) => {
+            const cursor = foodCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/myFoods', logger, verifyToken, async (req, res) => {
+            if (req.user?.email !== req.query?.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email }
@@ -147,7 +177,16 @@ async function run() {
 
             const result = await foodCollection.updateOne(filter, food, options);
             res.send(result);
-        })
+        });
+
+
+        app.get('/searchFoods', async (req, res) => {
+            const { query } = req.query;
+            const cursor = foodCollection.find({ foodName: { $regex: query, $options: 'i' } });
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
 
 
         //Gallery Data api
@@ -170,12 +209,9 @@ async function run() {
             res.send(result);
         });
 
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");//remove this before sending to production
+
     } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
+        
     }
 }
 run().catch(console.dir);
